1eeabb559cd29626102f6dc75b608d3e63779279
unknown <v.frolenkov@andagar.ru>
2024-09-18 12:56:01 +0300

    geomix_app. initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelegramService } from './services/telegram.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <header>
+      <a href="https://geomix.ru/">Go to Geomix main page</a>
+      <a href="/">Home</a>
+    </header>
+    <router-outlet />
+  `,
 })
 export class AppComponent {
   title = 'geomix_app';
+  telegram = inject(TelegramService);
+
+  constructor() {
+    this.telegram.ready();
+  }
 }
